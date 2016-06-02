95420ee97a1b0afe392ed51bee1d9e542cb8694a
RaulJimenezOrtega <hhkaos@gmail.com>
2016-06-02 07:44:02 +0200

    Removing unnecesary files

--- examples/findAddressCandidates.js
+++
@@ -1,9 +0,0 @@
-var config = require('./config'),
-    ArcNode = require('arc-node'),
-    service = new ArcNode(config);
-
-service.findAddressCandidates({
-    address: "Emilio muñoz 35, madrid"
-}).then(function(response){
-    console.log("response: ", JSON.stringify(response, null, "\t"));
-});
